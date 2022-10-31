b76b2813849b27d182d1be0d2d677bdebb890117
Adam <adamprice4@gmail.com>
2022-10-31 17:57:26 +0000

    Reduced scale

--- card.py
+++ card.py
@@ -5,20 +5,19 @@ class Card():
     def __init__(self, index):
         self.suit_str_to_int = {'CLUBS': 0, 'DIAMONDS': 1, 'SPADES': 2, 'HEARTS': 3}
         self.value_str_to_int= {'ACE': 0, 'TWO': 1, 'THREE': 2, 'FOUR': 3,
-                                'FIVE': 4, 'SIX': 5, 'SEVEN': 6, 'EIGHT': 7, 'NINE': 8,
-                                'TEN': 9, 'JACK': 10, 'QUEEN': 11, 'KING': 12}
+                                'FIVE': 4, 'JACK': 5, 'QUEEN': 6, 'KING': 7}
         self.suit_int_to_str = {v: k for k, v in self.suit_str_to_int.items()}
         self.value_int_to_str = {v: k for k, v in self.value_str_to_int.items()}
 
-        self.suit = int(index / 13)
-        self.value = index % 13
+        self.suit = int(index / 6)
+        self.value = index % 6
 
         self.vec = np.zeros(52)
         self.vec[index] = 1
 
     def asvector(self):
-        vector = np.zeros(52)
-        index = self.suit * 13 + self.value
+        vector = np.zeros(32)
+        index = self.suit * 6 + self.value
         vector[index] = 1
         return vector
 
