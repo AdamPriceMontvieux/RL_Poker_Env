329452fa3fd2fa316f2d9c10f771db2fa56446b1
Adam <adamprice4@gmail.com>
2022-11-03 08:34:57 +0000

    Heurisitc agent work

--- agents/heuristic_policy.py
+++ agents/heuristic_policy.py
@@ -3,12 +3,14 @@ import numpy as np
 import random
 from ray.rllib.policy.policy import Policy
 from gym import spaces
+import mpu
 
 class HeuristicPolicy(Policy):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        matrix = np.load('../AITables/2_card_values_10000_norm.npy')
+        HandScores = mpu.io.read('filename.pickle')
+        print(args[2]['difficulty'])
 
     def get_initial_state(self):
         return [random.choice([0, 1, 2])]
@@ -25,8 +27,8 @@ class HeuristicPolicy(Policy):
     ):
 
         # values = matrix[obs_batch]
-
-        
+        obs = obs_batch[0][0:28] + obs_batch[0][29:]
+        print(obs)
         # if value > rand * 1.5:
         #     return 2 #Raise
         # elif value > rand:
@@ -34,6 +36,6 @@ class HeuristicPolicy(Policy):
         # else: 
         #     return 0 #Fold
 
-        return 0, [], {}
+        return [np.random.random_integers(0,3) for x in obs_batch], [], {}
 
 
