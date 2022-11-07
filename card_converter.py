e3d0b70c9f649781bd8c9937645eee9ea1906ef1
Adam <adamprice4@gmail.com>
2022-11-07 12:17:39 +0000

    more hand vals

--- card_converter.py
+++ card_converter.py
@@ -5,8 +5,7 @@ class CardConverter():
     def __init__(self):
         self.suit_str_to_int = {'CLUBS': 0, 'DIAMONDS': 1, 'SPADES': 2, 'HEARTS': 3}
         self.value_str_to_int= {'ACE': 0, 'TWO': 1, 'THREE': 2, 'FOUR': 3,
-                                'FIVE': 4, 'SIX': 5, 'SEVEN': 6, 'EIGHT': 7, 'NINE': 8,
-                                'TEN': 9, 'JACK': 10, 'QUEEN': 11, 'KING': 12}
+                                'FIVE': 4,'JACK': 5, 'QUEEN': 6, 'KING': 7}
         self.suit_int_to_str = {v: k for k, v in self.suit_str_to_int.items()}
         self.value_int_to_str = {v: k for k, v in self.value_str_to_int.items()}
 
