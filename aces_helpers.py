19228666c72251b3dcfa3755d455580211bd49d4
Aidan <aidan.jones@montvieux.com>
2022-11-10 16:42:19 +0000

    self play

--- aces_helpers.py
+++ aces_helpers.py
@@ -9,34 +9,100 @@ import numpy as np
 import ray
 from ray.rllib.models import MODEL_DEFAULTS
 from ray.rllib.policy.policy import PolicySpec
+from ray.rllib.policy.policy import Policy
+
 from ray.tune.registry import register_env
 import tensorflow as tf
 import torch
+
 
 heuristic_observation_space = spaces.Dict({
             "hand": spaces.Box(0, 1, shape=(24, )),
             "community": spaces.Box(0, 1, shape=(24, ))
         })
+
+PPO_Agent_observation_space = spaces.Dict({
+            "obs": spaces.Box(0, 400, shape=(24+24+16+4, )),
+            "state": spaces.Box(0, 1, shape=(1, )),
+        })
+
 action_space = spaces.Discrete(3)
 
-def create_env_etc(model):
+class TrainedPolicyAgent(Policy):
+
+    def __init__(self,*args, **kwargs):
+
+        super().__init__(*args, **kwargs)  
+        
+        self.model = torch.load(args[2]['policy_checkpoint_path']+'/model.pt')
+        self.model.eval()
+        
+    def get_initial_state(self):
+        return [0]
+        
+    def compute_actions(
+            self,
+            obs_batch,
+            state_batches=None,
+            prev_action_batch=None,
+            prev_reward_batch=None,
+            info_batch=None,
+            episodes=None,
+            **kwargs
+        ):
+        #return inference(is_training=tf.constant(False), observations=obs_batch, timestep=tf.constant(-1, dtype=tf.int64))['actions_0'][0]
+        dic = {'obs': torch.tensor(obs_batch.reshape(1, -1))}
+        return [torch.argmax(self.model(dic, [torch.tensor(np.zeros(0))], torch.tensor(np.zeros(1)))[0]).item()], [], {}
+
+    def get_weights(self):
+        return None
+
+    def set_weights(self, weights):
+        return None
+
+def get_heuristic_policy(difficulty=0):
+    return (HeuristicPolicy, heuristic_observation_space, action_space, {'difficulty': difficulty})
+    
+
+def create_env_etc(env_config):
+    agent_names = {}
+    policies = {}
+    policies_to_train = []
+    model = MODEL_DEFAULTS.update({'fcnet_hiddens': [512, 512], 'fcnet_activation': 'relu'})
+
+    for i in range(4):
+        agent = env_config['agent'+str(i)]
+        agent_type = agent['type']
+        agent_name = ''
+        if agent_type == 'Heuristic':
+            policies[i] = get_heuristic_policy(agent['difficulty'])
+            agent_name = 'Heuristic_'+str(i)
+        elif agent_type == 'ppo':
+            if 'policy_checkpoint_path' in agent:
+                policy_checkpoint_path = agent['policy_checkpoint_path']
+                print(policy_checkpoint_path)
+                policies[i] = (TrainedPolicyAgent, PPO_Agent_observation_space, action_space,{'policy_checkpoint_path' : policy_checkpoint_path})
+                #policies[i] = (TrainedPolicyAgent, PPO_Agent_observation_space, action_space, policy_checkpoint_path, {})
+            else:
+                policies[i] = PolicySpec(config={})
+            agent_name =  'ppo_'+str(i)
+        if 'train' in agent:
+            if agent['train'] is True:
+                policies_to_train.append(agent_name)
+        #TODO ensure agent_name is not blank
+        agent_names[i] = agent_name
+
+    
+
     def select_policy(agent_id, episode, **kwargs):
-    if agent_id == 0:
-        return "learned"
-    elif agent_id == 1:
-        return "Heuristic_10"
-    elif agent_id == 2:
-        return "Heuristic_100"
-    elif agent_id == 3:
-        return "Heuristic_1000"
-    #TODO throw exception.
-    return "Heuristic_1000"
+        return agent_names[agent_id]
 
     def env_creator(config):
         env = PokerEnv(select_policy, config)
         return env
 
     register_env("poker", lambda config: env_creator(config))
+
 
     config = (
         PPOConfig()
@@ -46,18 +112,13 @@ def create_env_etc(model):
         .environment(disable_env_checking=True)\
         .multi_agent(
             policies={
-                #These policies thave pre-definded polices that dont learn.
-                "random": PolicySpec(policy_class=RandomActions),
-                "Heuristic_10": (HeuristicPolicy, heuristic_observation_space, action_space, {'difficulty': 0}),
-                "Heuristic_100": (HeuristicPolicy, heuristic_observation_space, action_space, {'difficulty': 1}),
-                "Heuristic_1000": (HeuristicPolicy, heuristic_observation_space, action_space, {'difficulty': 2}),
-                #Passing nothing causes this agent to deafult to using a PPO policy
-                "learned": PolicySpec(
-                    config={}
-                ),
+                agent_names[0]: policies[0],
+                agent_names[1]: policies[1],
+                agent_names[2]: policies[2],
+                agent_names[3]: policies[3],
             },
             policy_mapping_fn=select_policy,
-            policies_to_train=['learned'],
+            policies_to_train=policies_to_train,
         )\
         .resources(num_gpus=0)\
         .framework('torch')
